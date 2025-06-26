d17cf0eb061793a8a08f2451fdc1fd01800f768e
Alex Muirhead <a.muirhead@uq.net.au>
2025-06-26 17:24:29 +1000

    Add simple line-reading

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
+use std::fs::File;
+use std::io::{self, BufRead};
+use std::path::Path;
+
+fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
+where
+    P: AsRef<Path>,
+{
+    let file = File::open(filename)?;
+    Ok(io::BufReader::new(file).lines())
+}
+
 fn main() {
-    println!("Hello, world!");
+    let Ok(lines) = read_lines("./thermo.inp") else {
+        eprintln!("Could not open file");
+        return;
+    };
+
+    for line in lines.map_while(Result::ok) {
+        // Skip comment lines
+        if line.starts_with("!") {
+            continue;
+        }
+        println!("{line}");
+    }
 }
